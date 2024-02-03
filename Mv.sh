47de54ff04b2de4d43f452a1b676461a02f937cc
Ankit <ankitnaik16@gmail.com>
2024-02-03 10:37:16 +0530

    added alacritty

--- Mv.sh
+++ Mv.sh
@@ -3,6 +3,7 @@
 read -rp "Enter type of installation native or flatpak? (native/flatpak): " install_type
 if [ install_type=native ]; then
 	cp -r mpv $HOME/.config/
+	cp -r alacritty $HOME/.config/
 else  
 	cp -r mpv $HOME/.var/app/io.mpv.Mpv/config/mpv/
 fi
