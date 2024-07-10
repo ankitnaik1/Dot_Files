2c4d6744831056549a46a2bd416a824281c273f5
ankitnaik1 <ankitnaik1@outlook.com>
2024-07-10 10:26:13 +0530

    changed struture

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo "Copying Files..."
-paru -S alacritty neovim hyprland hyprpaper hyprlock hypridle ags daart-sass ttf-material-symbols-variable-git
-cp -rf alacritty $HOME/.config/
-cp -rf nvim $HOME/.config/
-cp -rf hypr $HOME/.config/
-cp -rf ags $HOME/.config/
+paru -S alacritty neovim hyprland hyprpaper hyprlock hypridle ags daart-sass ttf-material-symbols-variable-git fuzzel
+cp -rf .config/alacritty $HOME/.config/
+cp -rf .config/nvim $HOME/.config/
+cp -rf .config/hypr $HOME/.config/
+cp -rf .config/ags $HOME/.config/
+cp -rf .config/fuzzel $HOME/.config/
+cp -rf .local/bin/fuzzel-emoji $HOME/.local/bin/
 read -rp "Enter type of installation native or flatpak? (native/flatpak): " install_type
 if [ install_type=native ]; then
 	cp -rf mpv $HOME/.config/
