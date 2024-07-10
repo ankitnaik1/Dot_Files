44126ead4f0870e8c569de52a8de5c0d0cbd5cc4
ankitnaik1 <ankitnaik1@outlook.com>
2024-07-10 10:32:11 +0530

    changed struture

--- nvim/init.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", {noremap=false})
---Number Set
-vim.opt.relativenumber = true
-vim.opt.number = true
---Mouse mode
-vim.opt.mouse = ""
---Lazy Package Manager
-require('plugins.lazy')
-require('plugins.obsidian')
