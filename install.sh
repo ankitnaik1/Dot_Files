99587878ce2bdaa04aac0dbd7c4a595983022cc6
ankitnaik1 <ankitnaik1@outlook.com>
2024-07-10 09:12:57 +0530

    change installed script

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo "Copying Files..."
+paru -S alacritty neovim hyprland hyprpaper hyprlock hypridle ags daart-sass ttf-material-symbols-variable-git
 cp -rf alacritty $HOME/.config/
 cp -rf nvim $HOME/.config/
 cp -rf hypr $HOME/.config/
+cp -rf ags $HOME/.config/
 read -rp "Enter type of installation native or flatpak? (native/flatpak): " install_type
 if [ install_type=native ]; then
 	cp -rf mpv $HOME/.config/
