b179519f4e327808fb877d95904dd849b1356278
Ankit Naik <ankitnaik1@outlook.com>
2024-09-16 14:07:29 +0530

    edited iptv links

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-read -rp "Install hyprland? (Yes/No): " hyprland
-if [ hyprland=yes ]; then
+read -rp "Install hyprland? (Y/N): " hyprland
+if [ "$hyprland" = "Y" ]; then
 	paru -S alacritty neovim hyprland hyprpaper hyprlock hypridle ags daart-sass ttf-material-symbols-variable-git fuzzel
 	cp -rf .config/hypr $HOME/.config/
 	cp -rf .config/ags $HOME/.config/
@@ -14,7 +14,7 @@ else
 fi
 echo "Copying Files..."
 read -rp "Enter type of installation native or flatpak? (native/flatpak): " install_type
-if [ install_type=native ]; then
+if [ "install_type" = "native" ]; then
 	cp -rf mpv $HOME/.config/
 else  
 	cp -rf mpv $HOME/.var/app/io.mpv.Mpv/config/mpv/
