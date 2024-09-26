7c695414cac670456f23c88f968dbcb2e16ed46a
ankitnaik1 <ankitnaik1@outlook.com>
2024-09-26 09:56:57 +0530

    updated alacritty and neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,14 @@
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", {noremap=false})
+--Color
+vim.api.nvim_set_hl(0, "Normal", { bg = "#282828", fg = "#fbf1c7" })
+vim.api.nvim_set_hl(0, "Comment", { fg = "#cc241d" })
+vim.api.nvim_set_hl(0, "Constant", { fg = "#458858" })
+vim.api.nvim_set_hl(0, "Identifier", { fg = "#b16286" })
+vim.api.nvim_set_hl(0, "Statement", { fg = "#d79921" })
+vim.api.nvim_set_hl(0, "PreProc", { fg = "#d79921" })
+vim.api.nvim_set_hl(0, "Type", { fg = "#d79921" })
+vim.api.nvim_set_hl(0, "Special", { fg = "#d79921" })
+vim.api.nvim_set_hl(0, "Underlined", { fg = "#d79921" })
 --Number Set
 vim.opt.relativenumber = true
 vim.opt.number = true
@@ -6,4 +16,4 @@ vim.opt.number = true
 vim.opt.mouse = ""
 --Lazy Package Manager
 require('plugins.lazy')
-require('plugins.obsidian')
+
