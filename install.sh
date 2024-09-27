bb8c3db1a484e2788a60e3ffd3aa1b2603d9ba84
ankitnaik1 <ankitnaik1@outlook.com>
2024-09-27 14:39:29 +0530

    updated etv links

--- install.sh
+++ install.sh
@@ -2,23 +2,23 @@
 read -rp "Install hyprland? (Y/N): " hyprland
 if [ "$hyprland" = "Y" ]; then
 	paru -S alacritty neovim hyprland hyprpaper hyprlock hypridle ags daart-sass ttf-material-symbols-variable-git fuzzel
-	cp -rf .config/hypr $HOME/.config/
-	cp -rf .config/ags $HOME/.config/
-	cp -rf .config/fuzzel $HOME/.config/
+	cp -rf config/hypr $HOME/.config/
+	cp -rf config/ags $HOME/.config/
+	cp -rf config/fuzzel $HOME/.config/
 	cp -rf .local/bin/fuzzel-emoji $HOME/.local/bin/
 else
-	cp -rf .config/nvim $HOME/.config/
-	cp -rf .config/alacritty $HOME/.config/
-	cp -rf  zsh $HOME/.zsh
-	cp -rf  zsh/zshrc $HOME/.zshrc
+	cp -rf config/nvim $HOME/.config/
+	cp -rf config/alacritty $HOME/.config/
+	cp -rf zsh $HOME/.zsh
+	cp -rf zsh/zshrc $HOME/.zshrc
 fi
 echo "Copying Files..."
 read -rp "Enter type of installation native or flatpak? (native/flatpak): " install_type
 if [ "install_type" = "native" ]; then
-	cp -rf mpv $HOME/.config/
-	cp -rf chromium-flags.conf $HOME/.config/
+	cp -rf config/mpv $HOME/.config/
+	cp -rf config/chromium-flags.conf $HOME/.config/
 else  
-	cp -rf mpv $HOME/.var/app/io.mpv.Mpv/config/
-	cp -rf chromium-flags.conf $HOME/.var/app/io.github.ungoogled_software.ungoogled_chromium/config/
+	cp -rf config/mpv $HOME/.var/app/io.mpv.Mpv/config/
+	cp -rf config/chromium-flags.conf $HOME/.var/app/io.github.ungoogled_software.ungoogled_chromium/config/
 fi
 echo "Done"
